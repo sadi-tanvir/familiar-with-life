3a9434a71706a4e191dd70ea37ef9f7353abe351
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-12 18:31:50 +0600

    added service section in the home page

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from "./Home.module.css"
+
+const Banner = () => {
+    return (
+        <div className={`w-full h-screen flex flex-col justify-center items-center ${classes.doctorBG}`}>
+            <div className="w-11/12 flex flex-col justify-start mt-0">
+                <h1 className="text-slate-700 font-bold -mt-44">
+                    Hi! my Name is Tanvir hossain Sadi <br />
+                    <span className="text-red-500 font-bold">
+                    I am a Experts in Heart Health
+                    </span>
+                </h1>
+                <small className="text-slate-600">MBBS (DMU), FCPS (Surgery) MD (USA), FCVS (USA), Advance Fellowship in Cardiovascular Surgery (Mayoclinic, USA)</small>
+            </div>
+            <button className="px-5 py-2 bg-red-500 text-white font-bold rounded-md mr-auto ml-12 mt-20">Make An Appointment</button>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Service = ({data}) => {
     const {img, name, fee} = data
     return (
-        <div className="px-6 py-5 shadow-md">
-            <img className="rounded-lg h-3/6 w-full" src={img} alt="" />
+        <div className="px-6 py-5 shadow">
+            <img className="rounded-lg h-3/6 w-full shadow-md" src={img} alt="" />
             <div className="mt-6">
                 <h1 className="text-slate-700">{name}</h1>
                 <h4 className="text-slate-700">Fee: {fee}</h4>
             </div>
-            <button className="px-5 py-2 bg-red-500 text-white w-full font-bold rounded-md mt-3">Make An Appointment</button>
+            <button className="px-5 py-2 bg-red-500 shadow-md text-white w-full font-bold rounded-md mt-3">Make An Appointment</button>
         </div>
     );
 };
